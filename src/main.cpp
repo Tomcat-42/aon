2188d306ca33617c5ff983ab33ed1a46918e5ed3
Pablo <pablohuggem@gmail.com>
2022-11-23 17:43:55 -0300

    feat(main): add dummy code on main for further testinf

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,18 @@
 #include <libaon/load.hpp>
 #include <stdio.h>
 #include <stdlib.h>
+#
 
-int main() {
-  printf("Hello, World!\n");
+void test_dump_load() {
+  std::vector<int> data = {1, 2, 3, 4, 5};
+  libaon::dump(data, "test_dump_load.aon");
+  auto loaded_data = libaon::load<int>("test_dump_load.aon");
+  for (auto &token : loaded_data) {
+    printf("%d\n", token);
+  }
+}
+
+int main() { 
+  test_dump_load();
   return 0;
 }
