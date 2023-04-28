d95f02297571a4b66fe20ba265b817c84a7910ef
Pablo <pablohuggem@gmail.com>
2023-04-28 12:41:27 -0300

    feat(pyaon.cpp): add hashing module to pyaon library

--- src/pyaon/pyaon.cpp
+++ src/pyaon/pyaon.cpp
@@ -4,10 +4,12 @@ namespace py = pybind11;
 
 void sorting(py::module_ &m);
 void knapsack(py::module_ &m);
+void hashing(py::module_ &m);
 
 PYBIND11_MODULE(pyaon, m) {
     m.doc() = "Interact with the AON (Andrezao Object Notation) file format";
 
     sorting(m);
     knapsack(m);
+    hashing(m);
 }
